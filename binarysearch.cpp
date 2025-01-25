d1419c2f821368f5e8582e6851f36dba825bb548
AnirudhGagneja <140795166+AnirudhGagneja@users.noreply.github.com>
2025-01-25 11:35:39 +0530

    linear search

--- binarysearch.cpp
+++ binarysearch.cpp
@@ -23,13 +23,25 @@ int binary(int a[], int n, int val)
     }
     return -1;
 }
+int linearsearch(int a[] , int count , int val){
+    for (int i = 0; i < count; i++)
+    {
+        if(a[i]==val){
+            return i;
+        }
+        /* code */
+    }
+    return -1;
+    
+}
 int main()
 {
     int a[]={2,3,4,5,6,7,8};
     int n = sizeof(a)/sizeof(a[0]);
     int val=8;
     if(binary(a,n,val)!=-1){
-        cout<<binary(a,n,val);
+        cout<<binary(a,n,val)<<endl;
     }
+    cout<<linearsearch(a,n,val);
     return 0;
 }
