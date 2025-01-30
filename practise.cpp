2afd234a520df48436c1a47f8b045a7e0cc9a89e
AnirudhGagneja <140795166+AnirudhGagneja@users.noreply.github.com>
2025-01-30 15:45:40 +0530

    star triangle pattern

--- practise.cpp
+++ practise.cpp
@@ -4,7 +4,8 @@ using namespace std;
 bool prime(int n)
 {
     // bool isprime=true;
-    if(n==0 || n==1){
+    if (n == 0 || n == 1)
+    {
         return false;
     }
     for (int i = 2; i <= n / 2; i++)
@@ -17,42 +18,49 @@ bool prime(int n)
     }
     return true;
 }
-int sumdiv3(int n){
-    int sum=0;
-    for(int i=1;i<=n;i++){
-        if(i%3==0){
-            sum=sum+i;
-            cout<<i<<" ";
+int sumdiv3(int n)
+{
+    int sum = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        if (i % 3 == 0)
+        {
+            sum = sum + i;
+            cout << i << " ";
         }
     }
     return sum;
 }
-int fact(int n){
-    int fac=1;
-    if(n==0 || n==1){
-        fac=1;
+int fact(int n)
+{
+    int fac = 1;
+    if (n == 0 || n == 1)
+    {
+        fac = 1;
     }
-    else{
-        for (int i = 2; i <=n; i++)
+    else
+    {
+        for (int i = 2; i <= n; i++)
         {
-            fac=fac*i;
+            fac = fac * i;
         }
-        
     }
     return fac;
 }
-void squarepattern(int count){
+void squarepattern(int count)
+{
 
-    int a=1;
-    char ch='A';
+    int a = 1;
+    char ch = 'A';
     for (int i = 0; i < count; i++)
     {
-        for(int j =0;j<count;j++){
-            cout<<ch++<<" ";
+        for (int j = 0; j < count; j++)
+        {
+            cout << ch++ << " ";
         }
-        cout<<endl;
+        cout << endl;
     }
-    
+
     // for (int i = 0; i <count; i++)
     // {
     //    for (int j = 1; j <=count; j++)
@@ -70,51 +78,52 @@ void squarepattern(int count){
     //    }
     //    cout<<endl;
     // }
-    
 }
 
-void trianglepattern(int count){
-    char ch='a';
-    for (int i = 0; i <count; i++)
+void trianglepattern(int count)
+{
+    char ch = 'a';
+    for (int i = 0; i < count; i++)
     {
-        int a=1;
-        for(int j=0;j<=i;j++){
-            cout<<a++;
+        int a = 1;
+        for (int j = 0; j <= i; j++)
+        {
+            cout << a++;
         }
         ch++;
-        cout<<endl;
+        cout << endl;
     }
     //  for (int i = 0; i <count; i++)
     // {
-        
+
     //     for(int j=0;j<=i;j++){
     //         cout<<ch;
     //     }
     //     ch++;
     //     cout<<endl;
     // }
-    
 }
-void revtripattern(int count){
-    int a=1;
-    for (int i = 1; i <=count; i++)
+void revtripattern(int count)
+{
+    int a = 1;
+    for (int i = 1; i <= count; i++)
     {
-        for (int j = 1; j <=i; j++)
+        for (int j = 1; j <= i; j++)
         {
-            cout<<a++;
+            cout << a++;
         }
         // for (int j = i; j >0; j--)
         // {
         //     cout<<j;
         // }
-        
-        cout<<endl;
+
+        cout << endl;
     }
-    
 }
-void invertedtriangle(int count){
+void invertedtriangle(int count)
+{
     // for(int i = 0 ; i<count ; i++){
-       
+
     //     for (int j = 0 ; j<i ; j++)
     //     {
     //         cout<<" ";
@@ -124,94 +133,132 @@ void invertedtriangle(int count){
     //     }
     //     cout<<endl;
     // }
-    char ch='A';
+    char ch = 'A';
     for (int i = 0; i < count; i++)
     {
-        
-        for (int j = 0; j <i; j++)
+
+        for (int j = 0; j < i; j++)
         {
-            cout<<" ";
+            cout << " ";
         }
-        for(int j = 0 ;j<count-i;j++){
-            cout<<ch;
+        for (int j = 0; j < count - i; j++)
+        {
+            cout << ch;
         }
-        cout<<endl;
+        cout << endl;
         ch++;
-        
     }
-    
 }
-void pyramidpattern(int count){
+void pyramidpattern(int count)
+{
     for (int i = 0; i < count; i++)
     {
-        int a=1;
-        int b=i;
+        int a = 1;
+        int b = i;
 
-        //spaces
-        for(int j = 0 ; j<count-i;j++){
-            cout<<" ";
-        }
-
-        //ntri1
-        for (int j = 0; j <=i; j++)
+        // spaces
+        for (int j = 0; j < count - i; j++)
         {
-            cout<<a++;
-            
-            
+            cout << " ";
         }
 
-        //tri2
+        // ntri1
+        for (int j = 0; j <= i; j++)
+        {
+            cout << a++;
+        }
+
+        // tri2
         for (int k = 0; k < i; k++)
-            {
-                cout<<b--;
-            }
-        cout<<endl;
-        
+        {
+            cout << b--;
+        }
+        cout << endl;
     }
-    
 }
-void hollowdiamond(int count){
+void hollowdiamond(int count)
+{
     for (int i = 0; i < count; i++)
     {
-        for (int j = 0; j < count-i-1 ; j++)
+        for (int j = 0; j < count - i - 1; j++)
         {
-           cout<<" ";
+            cout << " ";
         }
-        cout<<'*';
-        if(i!=0){
-        for (int j = 2*i-1; j >0; j--)
+        cout << '*';
+        if (i != 0)
         {
-            cout<<" ";
-            
-        }
-        
-                cout<<'*';
+            for (int j = 2 * i - 1; j > 0; j--)
+            {
+                cout << " ";
             }
-            
-        cout<<endl;
-        
+
+            cout << '*';
+        }
+
+        cout << endl;
     }
-    for (int i = 0; i <count-1; i++)
+    for (int i = 0; i < count - 1; i++)
     {
-        //spaces
-        for (int j = 0; j <i+1; j++){
-            cout<<" ";
-        }
-        //stars
-        cout<<'*';
-        if(i!=count-2){
-        for (int j = 0; j <2*(count-i)-5; j++)
+        // spaces
+        for (int j = 0; j < i + 1; j++)
         {
-            cout<<" ";
-            
+            cout << " ";
         }
-        cout<<'*';
+        // stars
+        cout << '*';
+        if (i != count - 2)
+        {
+            for (int j = 0; j < 2 * (count - i) - 5; j++)
+            {
+                cout << " ";
+            }
+            cout << '*';
         }
-        cout<<endl;
-        
+        cout << endl;
     }
-    
-    
+}
+//    *
+//   * *
+//  *   *
+// * * * *
+void startriangle(int count)
+{
+    for (int i = 0; i < count; i++)
+    {
+        for (int j = 0; j < count - i - 1; j++)
+        {
+            cout << " ";
+            /* code */
+        }
+        cout << "*";
+        if (i == count - 1)
+        {
+            cout << " ";
+            for (int j = 0; j < count - 1; j++)
+            {
+                cout << "*" << " ";
+                /* code */
+            }
+
+            /* code */
+        }
+        else
+        {
+            for (int j = 0; j < 2 * i - 1; j++)
+            {
+                cout << " ";
+                /* code */
+            }
+            if (i != 0)
+            {
+                cout << "*";
+            }
+        }
+
+        cout << endl;
+
+        /* code */
+    }
 }
 int main()
 {
@@ -230,7 +277,8 @@ int main()
     // revtripattern(4);
     // invertedtriangle(4);
     // pyramidpattern(4);
-    hollowdiamond(4);
+    // hollowdiamond(4);
+    startriangle(7);
 
     return 0;
 }
