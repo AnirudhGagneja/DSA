140a69f51f2cf4ef7963ecfb1ea84b8b59b51284
AnirudhGagneja <140795166+AnirudhGagneja@users.noreply.github.com>
2025-02-13 23:27:59 +0530

    questions

--- practise.cpp
+++ practise.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <math.h>
+#include <vector>
 using namespace std;
 
 bool prime(int n)
@@ -272,10 +273,131 @@ int poweroftwo(int count){
     
     return ans;
 }
-void singlenumber(vector<int> vec){
+void singlenumber(vector<int> &vec){
+    int ans=0;
+    for(int n: vec)
+    {
+        ans^=n;
+    }
+    cout<<ans<<endl;
+    int i=0;
+    for(int n:vec){
+
+        if (n==ans)
+        {
+            cout<<i<<endl;
+            /* code */
+        }
+        i++;
+    }
+}
+void revavector(vector <int> &vec){
+    int i=0,j=vec.size()-1;
+    vector<int> temp;
+    temp=vec;
+    while (j>=0)
+    {
+        vec[i++]=temp[j--];
+        /* code */
+    }
     
 }
-
+void subarray(int arr[] , int count){
+    
+    for (int st = 0; st < count; st++)
+    {
+        for(int end=st;end<count;end++){
+            for (int i = st; i < end; i++)
+            {
+                cout<<arr[i];
+                /* code */
+            }
+            cout<<" ";
+            
+            
+        }
+        /* code */
+        cout<<endl;
+    }
+    
+}
+int maxsubarraysum(int arr[],int n){
+    int maxsum=INT_MIN;
+    for (int st = 0; st < n; st++)
+    {
+        int currentsum = 0;
+        for(int end=st;end<n;end++)
+        {
+            currentsum+=arr[end];
+            maxsum= max(currentsum , maxsum);
+        }
+        /* code */
+    }
+    return maxsum;
+}
+int kadanestheorm(int arr[],int n){
+    int maxsum = INT_MIN;
+    int currentsum=0;
+    for (int i = 0; i < n; i++)
+    {
+        currentsum+=arr[i];
+        maxsum=max(currentsum,maxsum);
+        if (currentsum<0)
+        {
+            currentsum=0;
+            /* code */
+        }
+        
+        /* code */
+    }
+    return maxsum;
+}
+void pairsum(int arr[],int n,int target){
+    int i=0,j=n-1;
+    
+    while(i<j){
+        int pairsum=arr[i]+arr[j];
+        if (pairsum<target)
+        {
+            i++;
+            /* code */
+        }
+        else if (pairsum>target)
+        {
+            j--;
+            /* code */
+        }
+        else{
+            cout<<i<<" "<<j;
+            break;
+        }
+        
+        
+    }
+    
+}
+void majorityelement( vector <int> & vec){
+    //using brute force
+    int n=vec.size()/2;
+    for(int num : vec){
+        int freq=0;
+        for(int el:vec){
+            if (el==num)
+            {
+                freq++;
+                /* code */
+            }
+            
+        }
+        if (freq>n)
+        {
+            cout<<num<<endl;
+            break;
+            /* code */
+        }
+        
+    }
+}
 int main()
 {
     // int n = 15;
@@ -296,6 +418,19 @@ int main()
     // hollowdiamond(4);
     // startriangle(7);
     // cout<<poweroftwo(7)<<endl;
-
+    vector <int> vec = {3,2,3,2,2,2};
+    // singlenumber(vec);
+    // revavector(vec);
+    // for(int n:vec){
+    //     cout<<n<<endl;
+    // }
+    int arr[]={1,4,5,6,7,8};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    // int sum=7;
+    // subarray(arr,n);
+    // cout<<maxsubarraysum(arr,n)<<endl; //O(n^2)
+    // cout<<kadanestheorm(arr,n)<<endl;//O(n)
+    // pairsum(arr,n,9);
+    majorityelement(vec);
     return 0;
 }
