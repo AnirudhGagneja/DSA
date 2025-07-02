5946d53b154c151de08f7f7d5d8858f8959e15ae
AnirudhGagneja <140795166+AnirudhGagneja@users.noreply.github.com>
2025-07-02 11:52:23 +0530

    binary and queue

--- binarysearch.cpp
+++ binarysearch.cpp
@@ -1,66 +1,31 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-int binary(vector<int> &vac , int val)
+
+int binarysearch(vector <int> arr , int val)
 {
-    int first = 0, last = vac.size()-1;
-    
-    while (first<=last)
+    int n = arr.size();
+    int st=0,end=n-1;
+    while (st<=end)
     {
-        int mid = (first + last) / 2;
-        if (a[mid] == val)
+        int mid = st+ (end-st)/2;
+        if(arr[mid]==val)
         {
             return mid;
         }
-        else if (a[mid] < val)
+        else if (arr[mid]>val)
         {
-            first = mid + 1;
+            end=mid-1;
         }
-        else
-        {
-            last=mid-1;
-        }
-    }
-    return -1;
-}
-int linearsearch(int a[] , int count , int val){
-    for (int i = 0; i < count; i++)
-    {
-        if(a[i]==val){
-            return i;
+        else{
+            st=mid+1;
         }
         /* code */
     }
     return -1;
-    
 }
-int linearsearchvector(vector<int> &vac , int val){
-    int i=0;
-
-    for(int num : vac){
-        
-        if (num==val)
-        {
-            return i;
-            /* code */
-        }
-        i++;
-        
-    }
-    return -1;
-}
-int main()
-{
-    int a[]={2,3,4,5,6,7,8};
-    int n = sizeof(a)/sizeof(a[0]);
-    int val=8;
-    if(binary(a,n,val)!=-1){
-        cout<<binary(a,n,val)<<endl;
-    }
-    // cout<<linearsearch(a,n,val)<<endl;
-    // cout<<linearsearch(a,n,4);
-    // vector <int> vec = {2,4,5,345,7,24,5};
-    // int val=34567;
-    // cout<<linearsearchvector(vec, val)<<endl;
+int main() {
+    vector<int>arr ={2,3,5,6,7,8,12,23};
+    cout<<binarysearch(arr,8);
     return 0;
 }
---
+++ queue.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+class node 
+{
+int data;
+node * next;
+
+node(int val){
+    data = val;
+    next=NULL;
+}
+};
+
+class queue
+{
+    node * head;
+    node * tail;
+
+    queue(){
+        head= tail = NULL;
+    }
+    bool isempty(){
+        if (head==NULL && tail==NULL)
+        {
+            return true;
+            /* code */
+        }
+        return false;
+    }
+    void pop(){
+        if (isempty())
+        {
+            cout<<"overflow";
+            return;
+        }
+        if (head->next==NULL && tail->next==NULL)
+        {
+            int val = head.data;
+            head=NULL;
+            tail=NULL;
+            /* code */
+        }
+        
+    }
+};
+int main() {
+    return 0;
+}
