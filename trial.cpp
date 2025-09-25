a28c92acd7a298b5dc100f2ca0109089563fd455
AnirudhGagneja <140795166+AnirudhGagneja@users.noreply.github.com>
2025-09-26 00:07:14 +0530

    array

--- trial.cpp
+++ trial.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 using namespace std;
 int main(){
+int arr[5];
+for(int i =0 ;i<5;i++){
+    printf("Enter value at index %d:\n" , i);
+    scanf("%d" ,&arr[i]);
+}
+for(int i =0 ;i<5;i++){
+    printf("%d " , arr[i]);
+}
 
 
 
