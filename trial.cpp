720589a1982acd26bbe3255ee9568d42cb8c48a9
AnirudhGagneja <140795166+AnirudhGagneja@users.noreply.github.com>
2025-10-03 22:18:16 +0530

    c

--- trial.cpp
+++ trial.cpp
@@ -1,16 +1,14 @@
 #include <iostream>
 using namespace std;
-int main(){
-int arr[5];
-for(int i =0 ;i<5;i++){
-    printf("Enter value at index %d:\n" , i);
-    scanf("%d" ,&arr[i]);
-}
-for(int i =0 ;i<5;i++){
-    printf("%d " , arr[i]);
-}
 
-
-
-return 0;
+int main() {
+    int n =123;
+    int ans=0;
+    while(n!=0){
+        int r = n%10;
+        ans = ans*10 + r ;
+        n/=10;
+    }
+    printf("the reverse of the number is %d",ans);
+    return 0;
 }
