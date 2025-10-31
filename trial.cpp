68e2aa6f83e7a125fa096f8e8c2f41fb36cf823b
AnirudhGagneja <140795166+AnirudhGagneja@users.noreply.github.com>
2025-11-01 04:36:37 +0530

    hushh test codes

--- trial.cpp
+++ trial.cpp
@@ -1,62 +1,46 @@
 #include <iostream>
 #include <queue>
 using namespace std;
-class node
-{
+class node{
     public:
     int data;
     node* left;
     node* right;
-    node(int val)
-    {
+
+    node(int val){
         data = val;
-        left=right=NULL;
     }
 };
+// 1
+//2  3
+//  4  5
 static int idx=-1;
 node* buildtree(int preorder[]){
     idx++;
-    
     if(preorder[idx]==-1)return NULL;
     node* root = new node(preorder[idx]);
     root->left = buildtree(preorder);
     root->right = buildtree(preorder);
     return root;
 }
-void preorder(node* root)
-{
-    if(root==NULL){return;}
-
+void preorder(node* root){
+    if(root==NULL)return;
     cout<<root->data<<" ";
     preorder(root->left);
     preorder(root->right);
 
 }
-void levelorder(node* root)
-{
-    queue<node*> q;
-    q.push(root);
-    while(q.size()>0){
-        node* temp = q.front();
-        q.pop();
-        cout<<temp->data<<" ";
-        if(temp->left!=NULL)q.push(temp->left);
-        if(temp->right!=NULL)q.push(temp->right);
-    }
-}
-void seplevelorder(node* root)
-{
+void levelorder(node* root){
     queue<node*>q;
     q.push(root);
     q.push(NULL);
-    while(q.size()>0)
-    {
+    while(q.size()>0){
         node* temp = q.front();
         q.pop();
         if(temp==NULL){
             if(q.size()>0){
-                cout<<endl;
                 q.push(NULL);
+                cout<<endl;
                 continue;
             }
             else{
@@ -66,46 +50,23 @@ void seplevelorder(node* root)
         cout<<temp->data;
         if(temp->left!=NULL)q.push(temp->left);
         if(temp->right!=NULL)q.push(temp->right);
+
     }
 }
-int d =0;
-int height(node*root){
+int height(node* root){
     if(root==NULL)return 0;
-    int left = height(root->left);
-    int right = height(root->right);
-    d = max(d, left+right);
-    return max(left,right)+1;
+    return max(height(root->left), height(root->right)) +1;
+}
+int count(node* root){
 
 }
-int sum(node* root){
-    if(root==NULL)return 0;
-    int left = sum(root->left);
-    int right = sum(root->right);
-    return left+right+root->data;
-}
-bool isidentical(node* p , node* q){
-    if(p==NULL || q== NULL){
-        return p==q;
-    }
+/*
+ 2 3 n
+ 
 
-    return isidentical(p->left,q->left) && isidentical(p->right,q->right) && p->data == q->data;
-}
-bool issubtree(node* root , node* subroot){
-    if(root==NULL || subroot==NULL)return root==subroot;
-    return isidentical(root->left,subroot) || isidentical(root->right , subroot) || isidentical(root,subroot);
-}
-int diameter(node* root){
-    if(root==NULL)return 0;
-    int len = height(root->right)+height(root->left);
-    return max(max(diameter(root->left),len),diameter(root->right));
-}
-int diameterbyh(node* root){
-    height(root);
-    return d;
-}
-int topview(node* root){
-    
-}
+
+
+*/
 int main() {
    int preorderarr[] = {1,2,-1,-1,3,4,-1,-1,5,-1,-1};
    node* root = buildtree(preorderarr);
@@ -113,10 +74,10 @@ int main() {
 //    preorderarr[0]=3;
    node* q = buildtree(preorderarr);
 //    cout<<root->data;
-//     preorder(root);
-    // levelorder(root);
+    // preorder(root);
+    levelorder(root);
     // seplevelorder(root);
     // cout<<sum(root);
-    cout<<diameterbyh(root);
+    // cout<<diameterbyh(root);
     return 0;
 }
