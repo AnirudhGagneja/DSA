cf4eccb46753d30510e8e7f437c30c2482a92dfa
AnirudhGagneja <140795166+AnirudhGagneja@users.noreply.github.com>
2025-11-14 16:34:48 +0530

    revison

--- trial.cpp
+++ trial.cpp
@@ -30,14 +30,17 @@ void preorder(node *root){
     preorder(root->left);
     preorder(root->right);
 }
-
+int height(node * root){
+    if(root==NULL)return 0;
+    return max(height(root->left) ,height(root->right))+1;
+}
 int main() {
    int preorderarr[] = {1,2,-1,-1,3,4,-1,-1,5,-1,-1};
    node* root = buildtree(preorderarr);
    idx=-1;
 //    preorderarr[0]=3;
    node* q = buildtree(preorderarr);
-//    cout<<root->data;
+   cout<<height(root);
     preorder(root);
     // levelorder(root);
     // seplevelorder(root);
